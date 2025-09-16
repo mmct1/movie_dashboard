949c2a4075291d7e24eb67d30e69c586bf268988
Mary <144135911+mmct1@users.noreply.github.com>
2025-09-16 00:47:36 +0000

    trying to create treemap

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd 
 import matplotlib.pyplot as plt
+import altair as alt
 
 st.set_page_config(
     page_title="ReelData",
@@ -13,11 +14,29 @@ df = pd.read_csv('./data/movie_ratings.csv')
 with st.sidebar:
     st.title('🍿 ReelData 🍿')
     
-    year_list = list(df.year.unique())[::-1]
+    year_list = list(df.rating_year.unique())[::-1]
     
     selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
-    df_selected_year = df[df.year == selected_year]
+    df_selected_year = df[df.rating_year == selected_year]
     df_selected_year_sorted = df_selected_year.sort_values(by="rating", ascending=False)
 
     color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+
+#genre breakdown
+def make_treemap(input_df, input_col, input_color_thm):
+    base = alt.Chart(input_df).encode(
+        x='count',
+        y=input_col,
+        text='count'
+    )
+    base.mark_bar() + base.mark_text(align='left', dx=2)
+    return base
+
+col = st.columns((3.5, 4.5), gap='medium')
+
+with col[0]:
+    st.markdown('#### Genre Breakdown')
+
+    treemap = make_treemap(df, 'genres',  selected_color_theme)
+    st.altair_chart(treemap, use_container_width=True)
