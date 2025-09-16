5d230852c36708a96f38c49898084b899804494c
Mary <144135911+mmct1@users.noreply.github.com>
2025-09-16 00:20:28 +0000

    adding sidebar to streamlit & doing intial data processing

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,17 @@ st.set_page_config(
     page_icon="🍿",
     layout="wide",
     initial_sidebar_state="expanded")
+
+df=pd.readcsv('./data/movie_ratings.csv')
+
+with st.sidebar:
+    st.title('🍿 ReelData 🍿')
+    
+    year_list = list(df.year.unique())[::-1]
+    
+    selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
+    df_selected_year = df[df.year == selected_year]
+    df_selected_year_sorted = df_selected_year.sort_values(by="rating", ascending=False)
+
+    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
+    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
